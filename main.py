06480afaab5465a8ac2435f130145a650d8b581d
28stockr <28stockr@bwschools.net>
2025-05-06 09:42:31 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,325 @@
+import turtle
+import random
+from idlelib.macosx import hideTkConsole
+
+screen = turtle.Screen()
+screen.screensize(500,500)
+screen.bgcolor('tan')
+t = turtle.Turtle()
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+t.write("ryan", font=("arial", 30, "italic"), align="center")
+
+
+#intro screen
+t.penup()
+t.goto(0,100)
+t.pendown()
+t.write("intro", font=("Ariel", 30, "italic"), align="center")
+
+import turtle
+import random
+from idlelib.macosx import hideTkConsole
+
+screen = turtle.Screen()
+screen.screensize(500,500)
+screen.bgcolor('red')
+t = turtle.Turtle()
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+t.write("ryan", font=("arial", 30, "italic"), align="center")
+
+
+#intro screen
+t.penup()
+t.goto(0,100)
+t.pendown()
+t.write("intro", font=("Ariel", 30, "italic"), align="center")
+
+round = input("press enter to continue: ")
+t.clear()
+screen.bgcolor('tan')
+
+t.penup()
+t.goto(0,-100)
+t.pendown()
+t.write("favorite foods", font=("Ariel", 30, "italic"), align="center")
+t.penup()
+t.write("", font=("Ariel", 30, "italic"), align="center")
+t.penup()
+t.goto(100,100)
+t.pendown()
+t.write("pizza", font=("Ariel", 30, "italic"), align="center")
+t.penup()
+
+t.penup()
+t.goto(200,500)
+t.pendown()
+t.write("fried chicken ", font=("Ariel", 30, "italic"), align="center")
+
+t.penup()
+t.goto(200,500)
+t.pendown()
+t.write("waffle ", font=("Ariel", 30, "italic"), align="center")
+t.penup()
+t.goto(-100,200)
+t.pendown()
+screen.addshape("food.gif")
+t.shape("food.gif")
+t.stamp()
+t.shape("classic")
+
+
+
+
+
+
+t.penup()
+t.goto(300,100)
+t.pendown()
+screen.addshape("food2.gif")
+t.shape("food2.gif")
+t.stamp()
+t.shape("classic")
+
+t.penup()
+t.goto(-200,0)
+t.pendown()
+#circle - radius
+t.pencolor("black")
+t.fillcolor("black")
+t.begin_fill()
+t.circle(50)
+t.end_fill()
+
+round = input("press enter to continue: ")
+t.clear()
+screen.bgcolor('orange')
+
+
+t.penup()
+t.goto(-200,0)
+t.pendown()
+t.pencolor("black")
+t.fillcolor("green")
+t.begin_fill()
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(50)
+t.end_fill()
+
+t.penup()
+t.goto(0,-100)
+t.pendown()
+t.write("hobbies", font=("Ariel", 30, "italic"), align="center")
+round = input("press enter to continue: ")
+t.clear()
+screen.bgcolor('lightblue')
+t.penup()
+t.goto(0,-100)
+t.pendown()
+t.write("favorite movie", font=("Ariel", 30, "italic"), align="center")
+t.pendown()
+screen.addshape("larry.gif")
+t.shape("larry.gif")
+t.stamp()
+t.shape("classic")
+
+
+
+t.penup()
+t.penup()
+t.goto(150,0)
+t.pendown()
+t.pencolor("blue")
+t.fillcolor("blue")
+t.begin_fill()
+t.goto(250,0)
+t.goto(200,100)
+t.goto(150,0)
+t.end_fill()
+
+round = input("press enter to continue: ")
+t.clear()
+screen.bgcolor('lightgreen')
+
+t.penup()
+t.goto(0,-100)
+t.pendown()
+t.write("favorite sport ", font=("Ariel", 30, "italic"), align="center")
+
+t.penup()
+t.goto(-100,200)
+t.pendown()
+screen.addshape("food.gif")
+t.shape("food.gif")
+t.stamp()
+t.shape("classic")
+
+
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.goto(25,-100)
+t.penup()
+t.goto(25,-100)
+t.setheading(0)
+t.pencolor("lightpink")
+t.fillcolor("lightpinl")
+t.begin_fill()
+t.goto(250,0)
+t.goto(200,100)
+t.goto(150,0)
+t.end_fill()
+
+turtle.done()
+
+
+round = input("press enter to continue: ")
+t.clear()
+screen.bgcolor('tan')
+
+t.penup()
+t.goto(0,-100)
+t.pendown()
+t.write("favorite foods", font=("Ariel", 30, "italic"), align="center")
+
+t.penup()
+t.pendown()
+screen.addshape(".gif")
+t.shape(".gif")
+t.stamp()
+t.shape("classic")
+
+t.goto(-200,0)
+t.pendown()
+#circle - radius
+t.pencolor("black")
+t.fillcolor("black")
+t.begin_fill()
+t.circle(50)
+t.end_fill()
+
+round = input("press enter to continue: ")
+t.clear()
+screen.bgcolor('gray')
+
+
+t.penup()
+t.goto(-200,0)
+t.pendown()
+t.pencolor("black")
+t.fillcolor("green")
+t.begin_fill()
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(50)
+t.end_fill()
+
+t.penup()
+t.goto(0,-100)
+t.pendown()
+t.write("hobbies", font=("Ariel", 30, "italic"), align="center")
+round = input("press enter to continue: ")
+t.clear()
+screen.bgcolor('lightblue')
+t.penup()
+t.goto(0,-100)
+t.pendown()
+t.write("favorite movie", font=("Ariel", 30, "italic"), align="center")
+
+t.penup()
+t.goto(0,100)
+t.pendown()
+screen.addshape("larry.gif")
+t.shape("larry.gif")
+t.stamp()
+t.shape("classic")
+
+
+
+t.penup()
+t.penup()
+t.goto(150,0)
+t.pendown()
+t.pencolor("blue")
+t.fillcolor("blue")
+t.begin_fill()
+t.goto(250,0)
+t.goto(200,100)
+t.goto(150,0)
+t.end_fill()
+
+round = input("press enter to continue: ")
+t.clear()
+screen.bgcolor('lightgreen')
+
+t.penup()
+t.goto(0,-100)
+t.pendown()
+t.write("favorite sport ", font=("Ariel", 30, "italic"), align="center")
+
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.goto(25,-100)
+t.penup()
+t.goto(25,-100)
+t.setheading(0)
+t.pencolor("lightpink")
+t.fillcolor("lightpinl")
+t.begin_fill()
+t.goto(250,0)
+t.goto(200,100)
+t.goto(150,0)
+t.end_fill()
+
+turtle.done()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
